fa1d82010a46579a99d3deffcb45c4c51c0af38b
Roman Sterlin <Feuse132@gmail.com>
2021-09-23 19:01:51 +0300

    -refactoring file names and locations

---
+++ SchedulerModels/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesModels
+{
+    public class Message 
+    {
+        public Guid MessageId { get; set; }
+
+        public string UserId { get; set; }
+
+        public int Likes { get; set; }
+
+        public Service Service { get; set; }
+
+        public DateTime Time { get; set; }
+        public MessageType Type { get; set; }
+        public object Price { get; set; }
+        public int Repeat { get; set; }
+    }
+}
---
+++ SchedulerModels/MessageType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesModels
+{
+    public enum MessageType : int
+    {
+        Likes = 1,
+        Pictures = 2
+    }
+}
